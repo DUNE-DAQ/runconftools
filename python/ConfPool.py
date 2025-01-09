2f6960364c9ffdec706db475ba4cbfff113402fd
Marco Roda <mroda@liverpool.ac.uk>
2025-01-09 18:41:55 +0100

    simple scheleton of ConfPool

---
+++ python/ConfPool.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from git import Repo
+
+class ConfPool :
+    def __init__( self,
+                  path,
+                  operation_url,
+                  base_url ) -> None :
+
+    def get_cods( self ) -> list :
+
+    def get_confs( self,
+                   regex ) -> list :
+
+    def propagate_cod( self,
+                       base_ref,
+                       key_regex ) -> list :
+
+    def tag( self,
+             base_ref,
+             key_regex ) -> list :
+
+    
