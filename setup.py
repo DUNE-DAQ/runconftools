70ac07c272ecc2dd5aae4edeaaf233311b7a00dd
Marco Roda <mroda@liverpool.ac.uk>
2025-03-11 10:51:38 +0100

    other setup

--- setup.py
+++ setup.py
@@ -2,24 +2,11 @@ from setuptools import setup
 
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
-    name="drunc",
+    name="config-management",
     install_requires=[
         "click",
         "click-shell",
-        "grpcio",
-        "googleapis-common-protos",
-        "grpcio-status",
-        "grpcio-tools",
-        "gunicorn",
-        "kafka-python",
-        "nest-asyncio",
-        "rich",
-        "requests",
-        "Flask",
-        "Flask-RESTful",
+        "GitPython",
         "sh",
-        "kubernetes",
-        "pytz",
-        "psutil",
     ],
-    extras_require={"develop": ["ipdb", "ipython", "ruff", "pre-commit", "pytest"]},
+    extras_require={"develop": ["ipdb", "ipython"]},
