55f57c26594c4e9be92d34831aad2a43107b5798
Marco Roda <mroda@liverpool.ac.uk>
2025-04-07 12:28:31 +0200

    Let the files to be removed to come from the repo instead of guessing

--- src/config_management/ConfPool.py
+++ src/config_management/ConfPool.py
@@ -194,23 +194,15 @@ class ConfPool:
         except :
             ## the configuraiton has no base, so it's not safe to delete anything
             return []
-        
-        safe_file = None
-        function_name = "get_entry_file"
+
+        to_be_removed = []
+        function_name = "irrelevant_files"
         if hasattr(base_module, function_name):
             functor=getattr(base_module, function_name)
-            safe_file_name=functor(self.repo.working_dir)
-            safe_file=Path(safe_file_name).name
+            to_be_removed=functor()
         else:
             ## again, too dangerous to remove things
             return []
-
-        logging.debug(f"file to be saved: {safe_file}")
-        to_be_removed = []
-        for f in directory.rglob("*.data.xml") :
-            logging.debug(f.name)
-            if f.name != safe_file :
-                to_be_removed.append(str(f.relative_to(self.repo.working_dir)))
 
         logging.debug("Removing " + ", ".join(to_be_removed))
         files = self.repo.index.remove(to_be_removed, r=True, working_tree=True)
