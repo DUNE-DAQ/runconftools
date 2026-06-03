ec950aa15d332c89fc96ca3c21423fd9dfd98bcd
Henry Wallace <henry.wallace@rhul.ac.uk>
2026-06-03 16:14:17 +0100

    Actually fix merge

--- src/runconftools/ConfPool.py
+++ src/runconftools/ConfPool.py
@@ -52,18 +52,18 @@ class ConfPool:
         except git.InvalidGitRepositoryError:
             logging.warning("The repo in %s is empty, cloning from remotes", path)
             self.repo = FetchTimedRepo.clone_from(url=base_url, to_path=path)
-        finally:
-            remotes = [r.name for r in self.repo.remotes]
-            if "base" not in remotes:
-                self.repo.create_remote(name="base", url=base_url)
-            if "operation" not in remotes:
-                self.repo.create_remote(name="operation", url=operation_url)
-            self.base = self.repo.remote("base", fetch_timeout=fetch_timeout)
-            self.operation = self.repo.remote("operation", fetch_timeout=fetch_timeout)
-            logging.info("Repo in %s set with the following remotes:", path)
-            for r in self.repo.remotes:
-                logging.info("%s -> %s", r.name, r.url)
-            sys.path.append(path + "/functions")
+
+        remotes = [r.name for r in self.repo.remotes]
+        if "base" not in remotes:
+            self.repo.create_remote(name="base", url=base_url)
+        if "operation" not in remotes:
+            self.repo.create_remote(name="operation", url=operation_url)
+        self.base = self.repo.remote("base", fetch_timeout=fetch_timeout)
+        self.operation = self.repo.remote("operation", fetch_timeout=fetch_timeout)
+        logging.info("Repo in %s set with the following remotes:", path)
+        for r in self.repo.remotes:
+            logging.info("%s -> %s", r.name, r.url)
+        sys.path.append(path + "/functions")
 
         ## this protection is necessary in case local base branch exist, example develop which is created by default
         ## In principle this could be done on any branch that does not respect the branch structure
